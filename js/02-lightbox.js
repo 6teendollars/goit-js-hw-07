8b3da5d100dc47e14e771cadce3120db0a9b4ebc
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-02-25 17:33:23 +0100

    task 2
    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+galleryRef.innerHTML = createMarkup(galleryItems);
+
+function createMarkup(array) {
+  return array
+    .map((e) => {
+      return `<li><a class="gallery__item" href="${e.original}">
+  <img class="gallery__image" src="${e.preview}" alt="${e.description}"/>
+</a></li>`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 200,
+});
+gallery.on("show.simplelightbox");
 
 console.log(galleryItems);

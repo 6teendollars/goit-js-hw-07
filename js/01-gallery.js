81b2c6e662415044ae98c686d860a10f0f174dba
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-02-26 09:36:16 +0100

    +text
    text

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,15 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from "./gallery-items.js"; //галерея
 // Change code below this line
 
-const galleryRef = document.querySelector(".gallery");
-const markup = craeteMarkup(galleryItems);
-galleryRef.innerHTML = markup;
+const galleryRef = document.querySelector(".gallery"); //выбираем елемент галереи
+const markup = craeteMarkup(galleryItems); // созданиу html разметки  которая принимает массив объектов galleryItems и возвращает строку с HTML-разметкой для каждого изображения в массиве.
+galleryRef.innerHTML = markup; //устанавливает содержимое элемента galleryRef равным markup, что отображает HTML-разметку галереи изображений на странице.
+
 function craeteMarkup(items) {
+  //функция createMarkup, которая создает HTML-разметку для каждого изображения в массиве items.
   return items
     .map(({ preview, original, description }) => {
+      //использует метод массива map(), чтобы создать массив строк с HTML-разметкой для каждого изображения ,используется деструктуризация объекта для извлечения значений свойств preview, original и description из каждого объекта в массиве items.
       return `
         <div class="gallery__item">
         <a class="gallery__link" href="${original}">
@@ -19,32 +22,37 @@ function craeteMarkup(items) {
       </a>
         </div>`;
     })
-    .join("");
+    .join(""); // чтобы объединить все элементы массива в одну строку
 }
 
 galleryRef.addEventListener("click", (e) => {
-  e.preventDefault();
-
+  //добавление слушателя на клик
+  e.preventDefault(); //отмены действия по умолчанию
   if (e.target.nodeName !== "IMG") {
+    //проверяется тип элемента, если не изображение то функция завершаеться
     return;
   } else {
     const instance = basicLightbox.create(
-      `<div class="modal"><img src="${e.target.dataset.source}" alt='${e.target.alt}'></div>`,
+      //если это изображение функция создает модальное окно
+      `<div class="modal"><img src="${e.target.dataset.source}" alt='${e.target.alt}'></div>`, //модальное окно с параметрами оригинального изображениея
       {
         onShow: (instance) => {
+          //создаем оброботчик события при открытии мод-окна
           window.addEventListener(
+            //добавляем слушателя на объект window, для отслеживания нажатия клавиши Escape
             "keydown",
             (event) => {
               if (event.code === "Escape") {
-                instance.close();
+                //если пользователь нажмет клавишу Escape
+                instance.close(); // ото закрываем модальное окно с помощью метода instance.close().
               }
             },
-            { once: true }
+            { once: true } //чтобы событие обработалось только один раз и не всплывало далее.
           );
         },
       }
     );
-    instance.show();
+    instance.show(); //чтобы показать модальное окно
   }
 });
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,23 +1,25 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryRef = document.querySelector(".gallery");
-galleryRef.innerHTML = createMarkup(galleryItems);
+const galleryRef = document.querySelector(".gallery"); // выбираем елемент галереи
+galleryRef.innerHTML = createMarkup(galleryItems); // созданиу html разметки  которая принимает массив объектов galleryItems и возвращает строку с HTML-разметкой для каждого изображения в массиве.
 
-function createMarkup(array) {
-  return array
+function createMarkup(items) {
+  return items
     .map((e) => {
-      return `<li><a class="gallery__item" href="${e.original}">
+      //перебирает массив, генерируя HTML-строку для каждого объекта.
+      return `<li><a class="gallery__item" href="${e.original}">  
   <img class="gallery__image" src="${e.preview}" alt="${e.description}"/>
 </a></li>`;
     })
-    .join("");
+    .join(""); // обеденяеться в одну строку
 }
 
-let gallery = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 200,
+let gallery = new SimpleLightbox(".gallery a", { //создаем екземпляр библиотеки SimpleLightbox
+  //селектор для ссылок галереи (".gallery a")
+  captionsData: "alt", //объект с параметрами , подпись
+  captionDelay: 200, // дилей для появления подписи
 });
-gallery.on("show.simplelightbox");
+gallery.on("show.simplelightbox"); //прослушивание слбытия, которое срабатывает при отображении изображения в галерее
 
 console.log(galleryItems);
